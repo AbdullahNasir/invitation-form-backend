77a3a9b939b0131e303d63b8e808ba3437af0fd2
Abdullah Nasir <abdullah555@yahoo.com>
2019-10-29 14:17:46 +0500

    mlab and email and password

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-exports.mongoDBUrl = 'mongodb://localhost:27017/invitation-form';
+// exports.mongoDBUrl = 'mongodb://localhost:27017/invitation-form';
+exports.mongoDBUrl = 'mongodb://admin:asdzxc123@ds141168.mlab.com:41168/invitation-db';
 
 exports.nodeMailerCreds = {
   user: process.env.EMAIL,
